872e15d22a7c3056cbcfbd8cf3ae217271cbeb70
Maximilian Goisser <goisser94@gmail.com>
2018-11-10 21:49:08 +0100

    feat(no_std): update to a decent up-to-date version of pwasm and nightly

--- rlay_ontology/build_features.sh
+++ rlay_ontology/build_features.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 cargo build
 cargo build --features web3_compat
-cargo +nightly-2018-11-08 build --no-default-features --features pwasm --target wasm32-unknown-unknown
+cargo +nightly-2018-10-15 build --no-default-features --features pwasm --target wasm32-unknown-unknown
